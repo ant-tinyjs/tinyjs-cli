c04b0d74d1b2a8d424e6a0ce24373ad88dacdfe1
yiiqii <yfj5tzl2005@sina.com>
2021-04-22 21:11:59 +0800

    1.4.0

--- config/commands.js
+++ config/commands.js
@@ -3,21 +3,29 @@
 module.exports = [{
   name: 'init',
   description: '初始化 Tiny.js 项目',
+  examples: 'tiny init',
   options: [],
 }, {
   name: 'plugin',
   description: '初始化 Tiny.js 插件库',
+  examples: 'tiny plugin',
   options: [],
 }, {
   name: 'resource',
   description: 'resources.js 生成器',
+  examples: 'tiny resource',
   options: [],
 }, {
   name: 'texture-compressor',
   description: '压缩纹理生成器',
+  examples: 'tiny texture-compressor <fold|file> [fold2|file] [-q <quality>] [-f <format>]',
   options: [{
     name: '-q, --quality <type>',
     description: '压缩率，可选值：low, medium, high',
     defaultValue: 'medium',
+  }, {
+    name: '-f, --format <type>',
+    description: '压缩纹理的类型，可选值：basis|astc|pvr（多个以,分割，不设置默认为所有）',
+    defaultValue: 'basis,astc,pvr',
   }],
 }];
